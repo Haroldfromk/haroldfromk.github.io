0814e6b88cc34ec5656d74330487a2464f60811b
Haroldfromk <dongik369@naver.com>
2024-02-29 17:29:50 +0900

    posts upload

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709183784',resources: [
+    cacheName: 'chirpy-1709195361',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
